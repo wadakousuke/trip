391cf4bfa3bace646e7d5c50b53f1551b91347b3
wadakousuke <100855797+wadakousuke@users.noreply.github.com>
2022-06-25 12:53:36 +0000

    [Modify]

--- app/javascript/packs/reviews.js
+++ app/javascript/packs/reviews.js
@@ -17,15 +17,15 @@ for(let i=0; i<stars.length; i++){
 		for(let j=0; j<stars.length; j++){
 			console.log(stars.length)
 			// まず全て星をくり抜く
-			stars[j].classList.remove("fas fa-star");
-			stars[j].classList.add("fas fa-star-o");
+			stars[j].classList.remove("fa-star");
+			stars[j].classList.add("fa-star-o");
 		}
 		// そのあと星の数だけ以下の関数が繰り返される。
 		for(let j=0; j<=i; j++){
 			console.log(stars.length)
-			stars[j].classList.remove("fas fa-star-o");
+			stars[j].classList.remove("fa-star-o");
 			// カーソルが乗った星まで星を塗りつぶす
-			stars[j].classList.add("fas fa-star");
+			stars[j].classList.add("fa-star");
 		}
 	})
 	// クリックされた星の番号+1をratingValueに代入
@@ -40,13 +40,13 @@ for(let i=0; i<stars.length; i++){
 		// まず5回繰り返す
 		for(let j=0; j<stars.length; j++){
 			// まず全ての星をくり抜く
-			stars[j].classList.remove("fas fa-star");
-			stars[j].classList.add("fas fa-star-o");
+			stars[j].classList.remove("fa-star");
+			stars[j].classList.add("fa-star-o");
 		}
 		for(let j=0; j<=index; j++){
 			// クリックされている星まで塗りつぶす
-			stars[j].classList.remove("fas fa-star-o");
-			stars[j].classList.add("fas fa-star");
+			stars[j].classList.remove("fa-star-o");
+			stars[j].classList.add("fa-star");
 		}
 	})
 }
