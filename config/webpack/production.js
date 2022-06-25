aecf80cac9ac8a534aa45c34cb31af51f09dc208
wadakousuke <100855797+wadakousuke@users.noreply.github.com>
2022-06-25 17:13:07 +0000

    [Modify]

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,9 +1,8 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
-const { environment } = require('@rails/webpacker')
+const environment = require('./environment')
 
-module.exports = environment
-
+module.exports = environment.toWebpackConfig()
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
     new webpack.ProvidePlugin({
